dd17c763125d11c295626982604654b65b01f913
willykar <103494595+willykar@users.noreply.github.com>
2024-09-06 00:43:15 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,48 +2,53 @@
 '''prime_game module'''
 
 
-def sieve_of_eratosthenes(n):
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    for i in range(2, int(n**0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    primes = [i for i in range(2, n + 1) if sieve[i]]
-    return primes
+def is_prime(n):
+     """ Checks if a number given n is a prime number """
+     for i in range(2, int(n ** 0.5) + 1):
+         if not n % i:
+             return False
+     return True
+
+
+def calculate_primes(n, primes):
+     """ Calculate all primes """
+     top_prime = primes[-1]
+     if n > top_prime:
+         for i in range(top_prime + 1, n + 1):
+             if is_prime(i):
+                 primes.append(i)
+             else:
+                 primes.append(0)
 
 
 def isWinner(x, nums):
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
 
-    def play_game(n):
-        prime_set = set(range(1, n + 1))
-        turn = 0
+    players_wins = {"Maria": 0, "Ben": 0}
 
-        for prime in primes:
-            if prime > n:
-                break
-            if prime in prime_set:
-                turn = 1 - turn  # Switch turns
-                multiples = set(range(prime, n + 1, prime))
-                prime_set -= multiples
+    primes = [0, 0, 2]
 
-        return 'Ben' if turn == 1 else 'Maria'
+    calculate_primes(max(nums), primes)
 
-    maria_wins = 0
-    ben_wins = 0
+    for round in range(x):
+        sum_options = sum((i != 0 and i <= nums[round]) for i in primes[:nums[round] + 1])
 
-    for n in nums:
-        winner = play_game(n)
-        if winner == 'Maria':
-            maria_wins += 1
+        if (sum_options % 2):
+            winner = "Maria"
         else:
-            ben_wins += 1
+            winner = "Ben"
 
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    else:
-        return None
+        if winner:
+            players_wins[winner] += 1
+
+    if players_wins["Maria"] > players_wins["Ben"]:
+        return "Maria"
+    elif players_wins["Ben"] > players_wins["Maria"]:
+        return "Ben"
+
+    return None
